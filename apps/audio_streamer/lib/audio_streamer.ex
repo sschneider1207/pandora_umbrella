78a41ec74cc117d864bfb5466b1a4f5e09de4f84
Sam Schneider <sds6065@gmail.com>
2015-11-18 22:23:16 -0500

    testing out bin data from stream

--- apps/audio_streamer/lib/audio_streamer.ex
+++ apps/audio_streamer/lib/audio_streamer.ex
@@ -10,22 +10,22 @@ defmodule AudioStreamer do
   ### Server ###
 
   def init({:ok, url}) do
+    file = File.open!("test.mp4", [:append])
     HTTPoison.start
     %{:id => id} = HTTPoison.get!(url, %{"Accept" => "audio/mpeg"}, [stream_to: self, recv_timeout: :infinity])
-    {:ok, %{id: id}}
+    {:ok, %{id: id, file: file}}
   end
 
   def handle_info(%{:__struct__ => HTTPoison.AsyncStatus, :id => msg_id}, %{:id => stream_id} = state) when stream_id === msg_id, do: {:noreply, state}
   def handle_info(%{:__struct__ => HTTPoison.AsyncHeaders, :id => msg_id}, %{:id => stream_id} = state) when stream_id === msg_id, do: {:noreply, state}
   def handle_info(%{:__struct__ => HTTPoison.AsyncEnd, :id => msg_id}, %{:id => stream_id} = state) when stream_id === msg_id, do: {:stop, :end_of_stream, state}
-  def handle_info(%{:__struct__ => HTTPoison.AsyncChunk, :chunk => chunk, :id => msg_id}, %{:id => stream_id} = state) when stream_id === msg_id do
-    IO.inspect chunk
-    :timer.sleep(100)
+  def handle_info(%{:__struct__ => HTTPoison.AsyncChunk, :chunk => chunk, :id => msg_id}, %{:id => stream_id, :file => file} = state) when stream_id === msg_id do
+    IO.binwrite(file, chunk)
     {:noreply, state}
   end
 
   def handle_info(_, state), do: {:noreply, state}
 
-  def terminate(:end_of_stream, _state), do: nil
+  def terminate(:end_of_stream, %{:file => file}), do: File.close(file)
   def terminate(_reason, _state), do: nil
 end
--- apps/audio_streamer/mix.exs
+++ apps/audio_streamer/mix.exs
@@ -33,6 +33,6 @@ defmodule AudioStreamer.Mixfile do
   #
   # Type "mix help deps" for more examples and options
   defp deps do
-    [{:httpoison, "~> 0.7.2"},]
+    [{:httpoison, "~> 0.7.2"}]
   end
 end
